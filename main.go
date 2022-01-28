c869472dae0d77cba8987cd2161398dd12c8ddea
vtlklr <vtlk_lr_ne@mail.ru>
2022-01-28 14:28:46 +0500

    test3

--- main.go
+++ main.go
@@ -31,10 +31,11 @@ func main() {
 	s, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	s = strings.TrimSpace(s)
 	s1 := strings.Split(s, " ")
-	if len(s1) == 1 && s1[0] == "" {
+	/*if len(s1) == 1 && s1[0] == "" {
 		fmt.Println(213)
 	} else {
 		fmt.Println(len(s1))
-	}
+	}*/
+	fmt.Println(len(s1))
 
 }
