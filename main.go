542d2e87fe5657ccdcafc297335c6e468384ab2e
vtlklr <vtlk_lr_ne@mail.ru>
2022-01-28 14:17:44 +0500

    test3

--- main.go
+++ main.go
@@ -4,20 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+/*
 func main() {
 	nums := readInput()
 
 	fmt.Println(nums)
+	os.Exit(0)
 }
 
 func readInput() int {
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
 	var a int
 	for scanner.Scan() {
+
 		a++
 	}
 	return a
+}
+*/
+func main() {
+	s, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	s = strings.TrimSpace(s)
+	s1 := strings.Split(s, " ")
+	if len(s1) == 1 && s1[0] == "" {
+		fmt.Println(0)
+	} else {
+		fmt.Println(len(s1))
+	}
+
 }
